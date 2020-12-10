12a1b003c0d247583fb32990e8861bcea1533588
Mohsen Rahnamaei <rahnamaei_158@yahoo.com>
2020-12-10 20:42:13 +0330

    Added newinfo and newitem functions

--- packages/hardhat/contracts/HealthZ.sol
+++ packages/hardhat/contracts/HealthZ.sol
@@ -7,19 +7,38 @@ import "hardhat/console.sol";
 contract HealthZ {
     // *** Structs ***
 
-    struct ContractItem {
-        address owner;
-        address contractAddress;
-        uint16 version;
+    struct Item {
+        address buyer;
+        address seller;
+        uint256 price;
+        bytes16 infoId;
+        bytes16 id;
+        uint256 deposit;
+        uint256 sellerDeposit;
+        uint256 buyerDeposit;
+        bool sellerConfirm;
+        bool buyerConfirmation;
+        uint256 endTime;
+    }
+    struct Info {
+        address creator;
+        bytes16 id;
+        // address owner;
+        // address contractAddress;
+        string detail;
+        bytes32 hash;
     }
 
     // *** Variable ***
     uint16 idMaterial = 0;
     string public purpose = "🛠 Programming Unstoppable Money";
 
-
     // *** Mapping ***
-    mapping(bytes32 => ContractItem) public Items;
+    mapping(bytes16 => Info) public infos;
+    bytes16[] public infosId;
+
+    mapping(bytes16 => Item) public items;
+    bytes16[] public itemsId;
 
     // Modifiers
     modifier onlyHospital {
@@ -31,7 +50,6 @@ contract HealthZ {
 
     event SetPurpose(address sender, string purpose);
 
-
     constructor() public {
         // uint id = getRandom();
         // what should we do on deploy?
@@ -39,27 +57,93 @@ contract HealthZ {
 
     function setPurpose(string memory newPurpose) public {
         //  console.log(randomId());
-        
+        // console.logBytes16(randomId());
         purpose = newPurpose;
         console.log(msg.sender, "set purpose to", purpose);
         emit SetPurpose(msg.sender, purpose);
     }
 
     // *** Getter Methods ***
+    function getInfoByIndex(uint256 i)
+        public
+        view
+        returns (
+            address creator,
+            bytes16,
+            // address owner;
+            // address contractAddress;
+            string memory,
+            bytes32
+        )
+    {
+        bytes16 id = infosId[i];
+        return (
+            infos[id].creator,
+            infos[id].id,
+            infos[id].detail,
+            infos[id].hash
+        );
+    }
 
     // *** Setter Methods ***
 
+    function newInfo(string memory detail)
+        public
+        returns (
+            bytes16 //TODO new modifier
+        )
+    {
+        // Info storage  i = infos[infoId];
+        bytes16 infoId = randomId();
+        // console.logBytes16(infoId);
+        Info storage i = infos[infoId];
+        i.detail = detail;
+        i.id = infoId;
+        i.creator = msg.sender;
+        infosId.push(infoId);
+        return infoId;
+    }
+
+    function newItem(
+        uint256 price,
+        bytes16 infoId,
+        uint256 deposit,
+        uint256 endTime // Buyer must call this function
+    )
+        public
+        returns (
+            bytes16 //TODO new modifier
+        )
+    {
+        // Info storage  i = infos[infoId];
+        bytes16 itemId = randomId();
+        // console.logBytes16(infoId);
+        Item storage i = items[itemId];
+        //i.detail=detail;
+        i.id = itemId;
+        i.buyer = msg.sender;
+        i.price = price;
+        i.infoId = infoId;
+        i.deposit = deposit;
+        i.endTime = endTime * 1 days;
+
+        itemsId.push(itemId);
+        return itemId;
+    }
+
     // *** Utility Functions ***
-    function randomId() public  returns (bytes32) {
-        bytes32 rand = keccak256(
-            abi.encodePacked(msg.sender, incrementIdMaterial())
+    function randomId() public returns (bytes16) {
+        bytes16 rand = bytes16(
+            keccak256(abi.encodePacked(msg.sender, incrementIdMaterial()))
             // abi.encodePacked(msg.sender, now, (block.number - 1))
         );
+        // console.log('hear');
+        // console.logBytes16(rand);
         return rand;
     }
 
     // * Increment and Get Id
-    function incrementIdMaterial() public  returns (uint16) {
+    function incrementIdMaterial() internal returns (uint16) {
         idMaterial = idMaterial + 1;
         return idMaterial;
     }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -22,5 +22,39 @@ describe("My Dapp", function () {
         expect(await myContract.purpose()).to.equal(newPurpose);
       });
     });
+    describe("randomId()", function () {
+      it("Should be able to set a new Info", async function () {
+        await myContract.randomId();
+      });
+    });
+    describe("newInfo()", function () {
+      it("Should be able to set a new Info", async function () {
+        const detail = "Test detail";
+        await myContract.newInfo(detail);
+        // await myContract.test(0).then((res)=>{
+        //   console.log(res)
+        // })
+        //  expect().to.equal("newPurpose");
+        //  console.log(a.data);
+
+        //await myContract.infos().with
+      });
+      describe("newItem()", function () {
+        it("Should be able to set a new Item from last recently added info", async function () {
+          await myContract.getInfoByIndex(0).then(async (res) => {
+            const detail = "Test detail";
+            const price = 100;
+            const infoId = res[1];
+            const deposit = 10;
+            const endTime = 5; //is day
+            await myContract.newItem(price, infoId, deposit, endTime);
+          });
+          //  expect().to.equal("newPurpose");
+          //  console.log(a.data);
+
+          //await myContract.infos().with
+        });
+      });
+    });
   });
 });
